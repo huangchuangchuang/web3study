989e4431eea582ca345354d91223e469add8829f
huangchuangchaung <17603070086@163.com>
2025-11-02 15:57:43 +0800

    upgrade test js

--- solidity_base/task_3/test/auction.js
+++ solidity_base/task_3/test/auction.js
@@ -4,21 +4,21 @@ const {AuctionTestHelper} = require("./utils/AuctionTestHelper");
 describe("Test auction", async function () {
 
     it("bid two", async function () {
-        const cont_used = new AuctionTestHelper();
-        await cont_used.setup();
-        await cont_used.auctionCreate();
-        await cont_used.placeEthBid("0.01");
-        await cont_used.placeUsdBid("101");
-        await cont_used.endAuction();
+        const used = new AuctionTestHelper();
+        await used.setup();
+        await used.auctionCreate();
+        await used.placeEthBid("0.01");
+        await used.placeUsdBid("101");
+        await used.endAuction();
     });
     it("bid three", async function () {
-        const cont_used = new AuctionTestHelper();
-        await cont_used.setup();
-        await cont_used.auctionCreate();
+        const used = new AuctionTestHelper();
+        await used.setup();
+        await used.auctionCreate();
         // 竞价必须大于当前最高价；否则合约会 revert
-        await cont_used.placeEthBid("0.01");
-        await cont_used.placeUsdBid("101");
-        await cont_used.placeEthBid("0.011");
-        await cont_used.endAuction();
+        await used.placeEthBid("0.01");
+        await used.placeUsdBid("101");
+        await used.placeEthBid("0.011");
+        await used.endAuction();
     });
 })
--- solidity_base/task_3/test/upgrade.js
+++ solidity_base/task_3/test/upgrade.js
@@ -3,20 +3,20 @@ const {AuctionTestHelper} = require("./utils/AuctionTestHelper");
 
 describe("Test upgrade", async function () {
   it("Upgrade 1", async function () {
-    const cont_used = new AuctionTestHelper();
-    await cont_used.setup();
-    await cont_used.auctionCreate();
-    // await cont_used.placeEthBid("0.01");
-    // await cont_used.placeUsdBid("101");
-    // await cont_used.endAuction();
-    await cont_used.upgradeNftAuction();
-    await cont_used.verifyNftAuctionUpgrade();
+    const used = new AuctionTestHelper();
+    await used.setup();
+    await used.auctionCreate();
+    // await used.placeEthBid("0.01");
+    // await used.placeUsdBid("101");
+    // await used.endAuction();
+    await used.upgradeNftAuction();
+    await used.verifyNftAuctionUpgrade();
   });
   // it("Upgrade 2", async function () {
-  //   const cont_used = new AuctionTestHelper();
-  //   await cont_used.setup();
-  //   await cont_used.auctionCreate();
-  //   await cont_used.upgradeNftAuction();
-  //   await cont_used.verifyNftAuctionUpgrade();
+  //   const used = new AuctionTestHelper();
+  //   await used.setup();
+  //   await used.auctionCreate();
+  //   await used.upgradeNftAuction();
+  //   await used.verifyNftAuctionUpgrade();
   // });
 });
